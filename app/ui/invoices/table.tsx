21980e9b6197a84c562becd2919dfacd116a2483
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2023-11-15 12:40:30 +0800

    Fix an issue in table.tsx

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -28,7 +28,6 @@ export default async function InvoicesTable({
                     <div className="mb-2 flex items-center">
                       <Image
                         src={invoice.image_url}
-                        alt="Oops..."
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
@@ -88,6 +87,7 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
+                        alt={`${invoice.name}'s profile picture`}
                         className="rounded-full"
                         width={28}
                         height={28}
